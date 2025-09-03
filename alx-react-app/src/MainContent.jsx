f6da8d9d9f39ec772eafeaf9af85306cbb1406c7
Joyclare <cheronojoyclaire@gmail.com>
2025-09-03 21:55:10 +0300

    Objective: Learn to create and assemble multiple specific React components within a main application component.

---
+++ alx-react-app/src/MainContent.jsx
@@ -0,0 +1,11 @@
+// MainContent.jsx
+function MainContent() {
+    return (
+      <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
+  
