aa525aa36a921d7180e0aabbc26815bf2217b079
Joyclare <cheronojoyclaire@gmail.com>
2025-09-25 22:28:45 +0300

    Restructure repo: keep only Tailwind project

--- alx-react-app-new/alx-react-app/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
-      <p>&copy; 2025 My Favorite Cities</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- alx-react-app-new/alx-react-app/src/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-  return (
-    <main style={{ padding: '20px', backgroundColor: '#eef', textAlign: 'center' }}>
-      <p style={{ fontSize: '18px', color: '#333' }}>Here are some of my favorite cities to visit:</p>
-    </main>
-  );
-}
-
-export default MainContent;
